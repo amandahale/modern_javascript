095d599015144e0df576b8e0456bab86d9f233d6
Amanda <amanda@actualize.co>
2021-03-23 12:37:19 -0500

    adds mathjs to main.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var moment = require('moment'); 
+var math = require('mathjs');
 
 console.log("Hello from Js!");
 var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(datetime);
 
+console.log(math.sqrt(-4));
